570f6df9d6a22556282b847710c50f63e57afbbf
Ömer Akgül <oemer.akguel@gmx.de>
2024-10-01 14:04:17 +0200

    created aufgabe1.py

---
+++ aufgabe1.py
@@ -0,0 +1,11 @@
+#def variablen
+
+first_name = "Ömer"
+last_name = "Akgül"
+birth_year = 1980
+has_license = True
+
+print("Vorname:",first_name)
+print("Nachname:",last_name)
+print("Geburtsjahr:",birth_year)
+print("Führerschein:",has_license)
